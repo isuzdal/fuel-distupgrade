0340272ee578aefac8c64e3556dc8d68c2788b22
Ivan Suzdal <isuzdal@mirantis.com>
2016-03-15 12:47:09 +0300

    [change] Mass changes related to 8.0

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 mkdir -p /usr/share/fuel-distupgrade
+
+PWD=$(dirname $0)
 install -m 644 functions /usr/share/fuel-distupgrade
-install -m 644 mos7.0-base.whitelist /usr/share/fuel-distupgrade
+install -m 644 mos8.0-base.whitelist /usr/share/fuel-distupgrade
 install -m 755 fuel-distupgrade /usr/sbin
 
 mkdir -p /etc/fuel
 for f in *.settings; do
-  install -m 644 $f /etc/fuel
+    install -m 644 $f /etc/fuel
 done
